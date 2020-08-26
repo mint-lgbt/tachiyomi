83b5954fb86ec6bd36e2751a4a5209c81c0272b3
Luna <63075513+lunaisnotaboy@users.noreply.github.com>
2020-08-26 10:31:47 -0400

    Create TachiyomiApplicationTests.kt

---
+++ src/test/kotlin/com/lunaisnotaboy/tachiyomi/TachiyomiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lunaisnotaboy.tachiyomi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TachiyomiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
